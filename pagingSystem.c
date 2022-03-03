8775ac6b2b946cc4d3a7dae9c79462a85b06739d
Joshuua Mo <39536803+gandumb@users.noreply.github.com>
2022-03-02 23:17:52 -0600

    Put in comments to guide me tomorrow

---
+++ pagingSystem.c
@@ -0,0 +1,16 @@
+
+
+
+//Prompt user for page replacement algorithm (FIFO, LRU, Second Chance)
+
+//Read a number delimited by spaces from the reference page file
+
+//Create a "memory" space of four pages of memory, with 4 pages of most recently removed from memory below it
+
+//Apply the page replacement algorithm to the reference string data
+
+//Display memory at each time a page is loaded or removed
+
+//Show an indcator below the column where page faults occured
+
+//Load the page into memory
